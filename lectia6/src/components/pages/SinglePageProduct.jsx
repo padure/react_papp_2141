6f6c6f1a3742d1c9a277dc6e4ddedc612d4de198
padure <paduregheorghe7@gmail.com>
2024-10-31 11:56:39 +0200

    Lectia 6

--- lectia6/src/components/pages/SinglePageProduct.jsx
+++ lectia6/src/components/pages/SinglePageProduct.jsx
@@ -3,18 +3,34 @@ import { useParams } from 'react-router-dom';
 import {
   fetchProduct
 } from '@/services/product/productService.js';
+import Slider from "react-slick";
+//Style
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const SinglePageProduct = () => {
+  const settings = {
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    cssEase: "linear",
+    dots: true,
+    arrow: false
+  }
   const [product, setProduct] = useState({});
   let { id } = useParams();
   useEffect(() => {
-    fetchProduct(id).then(result => setProduct({...result}));
-  });
+    fetchProduct(id).then(result => setProduct({ ...result }));
+  }, []);
   return (
-    <div>
+    <div className='container'>
       <h1>{product.title}</h1>
-      <div>
-        {product.images.map(image => (<img src={image} alt={product.title} className='w-64 shadow' />))}
+      <div className='w-1/4 h-72 mb-8'>
+        <Slider {...settings}>
+          {product.images?.map((image, index) => (
+              <img key={index} src={image} alt={product.title} className='w-12 h-64' />
+          ))}
+        </Slider>
       </div>
     </div>
   )
