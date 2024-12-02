c2f691d958c04a2d7b703844f9c396be00e9489a
padure <paduregheorghe7@gmail.com>
2024-12-02 08:16:13 +0200

    DB JSON

---
+++ lectia8/src/components/ProjectList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProjectList = () => {
+  return (
+    <div className='container px-8 m-auto'>
+        <h1 className='font-bold text-xl text-center py-5'>Lista proiectelor</h1>
+        
+    </div>
+  )
+}
+
+export default ProjectList;
--- lectia8/src/pages/Projects.jsx
+++ lectia8/src/pages/Projects.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Nav from '../components/Nav';
+import ProjectList from '../components/ProjectList';
 
 const Projects = () => {
   return (
     <div>
-      
+      <Nav />
+      <ProjectList />
     </div>
   )
 }
 
-export default Projects
+export default Projects;
