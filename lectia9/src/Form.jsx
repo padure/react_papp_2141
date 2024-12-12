b79d5605522737214bb8de167b6f962b704a71f3
padure <paduregheorghe7@gmail.com>
2024-12-12 12:57:09 +0200

    Lectia 9

--- lectia9/src/Form.jsx
+++ lectia9/src/Form.jsx
@@ -8,14 +8,19 @@ const Form = () => {
     const ageRef = useRef(null);
 
     const [employees, setEmployees] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-    useEffect(()=>{
+    useEffect(() => {
+        setLoading(true);
         axiosClient
             .get('/employees')
-            .then(({data})=>setEmployees(data))
-            .catch(()=>{})
+            .then(({ data }) => {
+                setEmployees(data);
+                setLoading(false);
+            })
+            .catch(() => setLoading(false));
     }, []);
-    
+
     const handleSubmit = (e) => {
         e.preventDefault();
         const name = nameRef.current.value;
@@ -23,7 +28,7 @@ const Form = () => {
         const age = ageRef.current.value;
         setEmployees((prevData) => [
             ...prevData,
-            {name, email, age}
+            { name, email, age }
         ]);
         //Save in JSON
         axiosClient.post('/employees', {
@@ -34,8 +39,8 @@ const Form = () => {
         ageRef.current.value = '';
     }
     return (
-        <div className='w-1/3 m-auto mt-14'>
-            <div className='shadow p-4 mb-8'>
+        <div className='mt-14'>
+            <div className='shadow p-4 mb-8 w-1/3 m-auto'>
                 <h1 className='text-lg font-bold'>Gestionarea angajatilor</h1>
                 <form onSubmit={handleSubmit}>
                     <div className="mb-3 flex flex-col gap-1">
@@ -50,29 +55,39 @@ const Form = () => {
                         <label htmlFor="age">Age:</label>
                         <input ref={ageRef} type="number" id='age' placeholder='Introduceti varsta' className='border p-1' />
                     </div>
-                    <button 
-                        type="submit" 
+                    <button
+                        type="submit"
                         className='bg-gray-700 py-1 px-4 text-white hover:bg-gray-500'
                     >Save</button>
                 </form>
             </div>
-            <h2 className='text-lg font-bold mb-4'>Lista angajatilor</h2>
-            {
-                employees.length > 0 ? (
-                    <ul>
-                        {employees.map(data => (
-                            <li key={data.id}
-                                className='p-2 border mb-1'
-                            >
-                                <b>Name:</b> {data.name} <br />
-                                <b>Email:</b> {data.email} <br />
-                                <b>Age:</b> {data.age}
-                            </li>
-                        ))}
-                    </ul>
-                ) : (
-                    <p>Nu avem angajati</p>
+            <div className='p-5'>
+                <h2 className='text-lg font-bold mb-4'>Lista angajatilor</h2>
+                {loading && (
+                    <div className="flex items-center align-middle bg-gray-800 text-white h-52">
+                        <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"></svg>
+                        Processing...
+                    </div>
                 )}
+                {
+                    !loading &&
+                        employees.length > 0 ? (
+                        <ul className='grid grid-cols-4 gap-3'>
+                            {employees.map(data => (
+                                <li key={data.id}
+                                    className='p-2 border mb-1'
+                                >
+                                    <b>Name:</b> {data.name} <br />
+                                    <b>Email:</b> {data.email} <br />
+                                    <b>Age:</b> {data.age}
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>Nu avem angajati</p>
+                    )
+                }
+            </div>
         </div>
     )
 }
